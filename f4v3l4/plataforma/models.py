88b80d704abe10c2f3f4904072171487769ac7e6
Edson Tomas <edson.transpioneira@gmail.com>
2022-04-28 15:00:04 -0300

    criação das classes model

--- f4v3l4/plataforma/models.py
+++ f4v3l4/plataforma/models.py
@@ -1,3 +1,37 @@
+from pickle import FALSE
 from django.db import models
 
-# Create your models here.
+class Enderecos (models.Model):
+    cep = models.CharField(max_length=8,null=FALSE)
+    logradouro = models.CharField(max_length=50,null=False)
+    numero = models.CharField(max_length=8)
+    bairro = models.CharField(max_length=30)
+    cidade = models.CharField(max_length=50)
+    uf = models.CharField(max_length=2)
+    
+class Contatos (models.Model):
+    tipo = models.CharField(max_length=15)
+    contato = models.CharField(max_length=20)
+
+class Categorias(models.Model):
+    categoria = models.CharField(max_length=30)
+
+class Servicos (models.Model):
+    desc_serv = models.CharField(max_length=20)
+    idCategoria  = models.ForeignKey(Categorias, null=False, on_delete=models.CASCADE)
+
+class Trabalhos (models.Model):
+    descricao = models.CharField(max_length=30)
+
+class Propostas (models.Model):
+    prazo = models.IntegerField()
+    valor = models.DecimalField(max_digits=2, decimal_places=2)
+    observacao = models.CharField(max_length=50)
+
+    
+    
+
+
+
+
+    
--- f4v3l4/usuario/models.py
+++ f4v3l4/usuario/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser 
 from django.db import models
+from plataforma.models import Enderecos , Contatos, Propostas, Trabalhos
 
 class Usuarios(AbstractUser):
-    bio = models.TextField(blank=True)
-    cadastrar = models.TimeField
+    idEndereco = models.ForeignKey(Enderecos, null=True, on_delete=models.CASCADE)
+    idContato  = models.ManyToManyField(Contatos)
+    idTrabalhos = models.ManyToManyField(Trabalhos)
+    idPropostas = models.ManyToManyField(Propostas)
+    
+    
