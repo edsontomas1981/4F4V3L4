269f6fd5180c205161fdef0c8f591a772f086074
Edson Tomas <edson.transpioneira@gmail.com>
2022-07-21 18:53:53 -0300

    Alteracaoe exclusao de propostas

--- f4v3l4/plataforma/views/prop_enviadas.py
+++ f4v3l4/plataforma/views/prop_enviadas.py
@@ -1,19 +1,33 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render,redirect
-from django.http.response import HttpResponse
 from Classes.home import Home
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.messages import constants
+from plataforma.models.Propostas import Propostas
 
-@login_required(login_url='/auth/login/')
-def prop_enviadas(request):
+def mostra_propostas(request):
     usuario=request.user
     pedido,imagens, prop_enviadas,propostaRecebidas = Home.gerarHome(usuario.id)
     paginator = Paginator(prop_enviadas, 5) # Mostra 5 por pagina
         
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+    return render(request,'./prop_enviadas.html',{'page_obj': page_obj})    
+
+@login_required(login_url='/auth/login/')
+def prop_enviadas(request):
     
     if request.method == "GET" :
-        return render(request,'./prop_enviadas.html',{'page_obj': page_obj})
+        return mostra_propostas(request)
     elif request.method == "POST" :
-        return render(request,'./prop_enviadas.html',{'page_obj': page_obj})
+        if 'exclui_proposta' in request.POST:
+            proposta=Propostas(id=request.POST.get('id'))
+            proposta.delete()
+            messages.add_message(request,constants.WARNING,'Proposta excluida !')
+            return mostra_propostas(request)
+        elif 'altera_proposta' in request.POST:
+            proposta=Propostas(id=request.POST.get('id'))
+            proposta.delete()
+            messages.add_message(request,constants.WARNING,'Proposta Alterada !')
+            return mostra_propostas(request)
---
+++ f4v3l4/templates/static/js/excluirPropostas.js
@@ -0,0 +1,8 @@
+function excluiProposta(id){
+    proposta=document.getElementById('excluir')
+    proposta.value=id
+}
+function alteraProposta(id){
+    proposta=document.getElementById('excluir')
+    proposta.value=id
+}
