5039deb4310c0a86f559f921f2e821904c613cf8
Edson <edsontomasdev@gmail.com>
2022-07-21 22:30:12 -0300

    Inicio alteração propostas

--- f4v3l4/templates/static/js/excluiContatos.js
+++ f4v3l4/templates/static/js/excluiContatos.js
@@ -1,11 +1,11 @@
-function excContato(idExc){
-    
+function excContato(idExc) {
+
     var id = document.getElementById('excluiId');
     var contato = document.getElementById('excluiContato');
     var tipo = document.getElementById('excluiTipo');
 
     id.value = idExc;
-    contato.value = document.getElementById('contato'+idExc).textContent;
-    tipo.value = document.getElementById('tipo'+idExc).textContent;
+    contato.value = document.getElementById('contato' + idExc).textContent;
+    tipo.value = document.getElementById('tipo' + idExc).textContent;
     contato.ariaDisabled = True;
 }
--- f4v3l4/templates/static/js/excluirPropostas.js
+++ f4v3l4/templates/static/js/excluirPropostas.js
@@ -1,8 +1,14 @@
-function excluiProposta(id){
-    proposta=document.getElementById('excluir')
-    proposta.value=id
+function excluiProposta(id) {
+    proposta = document.getElementById('excluir')
+    proposta.value = id
 }
-function alteraProposta(id){
-    proposta=document.getElementById('excluir')
-    proposta.value=id
+
+function alteraProposta(id) {
+    let valor = "valor" + id
+    let obs = "observacao" + id
+    let datIni = "datIni" + id
+    let prazo = "prazo" + id
+    console.log(valor, obs, datIni, prazo)
+    proposta = document.getElementById('excluir')
+    proposta.value = id
 }
