157ca49ff54b72140cd564177f1d1da6ca636b30
Edson Tomas <edson.transpioneira@gmail.com>
2022-07-22 21:37:49 -0300

    atualizacao propostas

--- f4v3l4/templates/static/js/excluirPropostas.js
+++ f4v3l4/templates/static/js/excluirPropostas.js
@@ -3,12 +3,28 @@ function excluiProposta(id) {
     proposta.value = id
 }
 
+function mes(strMes){
+    let mes
+}
+
+function separaData(data) {
+     mes 
+
+}
+
 function alteraProposta(id) {
-    let valor = "valor" + id
-    let obs = "observacao" + id
-    let datIni = "datIni" + id
-    let prazo = "prazo" + id
-    console.log(valor, obs, datIni, prazo)
-    proposta = document.getElementById('excluir')
+    let valor = document.getElementById("valor")
+    let obs = document.getElementById("obs")
+    let datIni = document.getElementById("prevInicio")
+    let prazo = document.getElementById("prazo")
+    let data =document.getElementById("datIni" + id).value
+    console.log(typeof(data))
+
+    valor.value = document.getElementById("valor" + id).value
+    obs.value = document.getElementById("obs" + id).value
+    prazo.value = document.getElementById("prazo" + id).value
+    datIni.value = Date.parse(data)
+    console.log(Date.parse(data))
+    proposta = document.getElementById('alterar')
     proposta.value = id
 }
